d5c73d887af90159fdec8ec80651fd36c293109b
이도현 <ksmfou98@naver.com>
2022-02-15 02:06:50 +0900

    feat: categoryList 불러오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,28 +2,38 @@ import Header from "components/common/Header";
 import type { NextPage } from "next";
 import { getMainCategoryList } from "utils/api";
 import { MainCategoryList } from "types/response";
+import styled from "styled-components";
+import GridCardList from "components/common/GridCardList";
 
 interface HomeProps {
   categoryList: MainCategoryList;
 }
 
 const Home: NextPage<HomeProps> = ({ categoryList }) => {
-  console.log(categoryList);
   return (
     <>
       <Header title="니콘내콘" leftIcon="hamburger" />
+      <CategoryListBox>
+        {/* <GridCardList list={categoryList} /> */}
+        {categoryList.conCategory1s.map((item) => (
+          <div key={item.id}>{item.id}</div>
+        ))}
+      </CategoryListBox>
     </>
   );
 };
 
 export const getStaticProps = async () => {
   const data = await getMainCategoryList();
-
   return {
     props: {
-      categoryList: data.conCategory1s,
+      categoryList: data,
     },
   };
 };
+
+const CategoryListBox = styled.div`
+  padding: 17px;
+`;
 
 export default Home;
