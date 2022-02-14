bc8225d5100ae43d40979436cd858d36e231c7e1
이도현 <ksmfou98@naver.com>
2022-02-14 21:41:09 +0900

    fix: Header import 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import Header from "components/common/Header";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
@@ -9,7 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Header title="땡철이" />
       <Component {...pageProps} />
     </ThemeProvider>
   );
