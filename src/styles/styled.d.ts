c2b2d3e0a1b2d87b0f6c4a69c212def48c598455
perfumelim <perfumelim@gmail.com>
2022-02-15 18:30:53 +0900

    refactor: 중복된 import 제거

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,6 +1,10 @@
 import "styled-components";
-import { ColorsTypes, FontSizeTypes, ZindexedTypes } from "./theme";
-import { breakPointTypes, ColorsTypes, FontSizeTypes } from "./theme";
+import {
+  breakPointTypes,
+  ColorsTypes,
+  FontSizeTypes,
+  ZindexedTypes,
+} from "./theme";
 
 declare module "styled-components" {
   export interface DefaultTheme {
