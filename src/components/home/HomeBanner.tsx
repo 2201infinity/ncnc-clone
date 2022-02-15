f08bd658767419f6e75028e1c586b0722d53f3ee
이도현 <ksmfou98@naver.com>
2022-02-15 15:29:25 +0900

    feat: HomeBanner UI 스타일링

--- src/components/home/HomeBanner.tsx
+++ src/components/home/HomeBanner.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import styled from "styled-components";
+
+const data = [
+  {
+    id: 1,
+    imageUrl: "/images/bannerImage1.png",
+  },
+  {
+    id: 2,
+    imageUrl: "/images/bannerImage1.png",
+  },
+  {
+    id: 3,
+    imageUrl: "/images/bannerImage1.png",
+  },
+];
 
 function HomeBanner() {
-  return <div>HomeBanner</div>;
+  return (
+    <BannerContainer>
+      <ImageBox>
+        <ImageList>
+          {data.map((item) => (
+            <BannerImage key={item.id} src={item.imageUrl} />
+          ))}
+        </ImageList>
+      </ImageBox>
+    </BannerContainer>
+  );
 }
+
+const BannerContainer = styled.div`
+  width: 100%;
+  height: 282px;
+  position: relative;
+`;
+
+const ImageBox = styled.div`
+  width: 100%;
+  position: absolute;
+  overflow: hidden;
+`;
+
+const ImageList = styled.div`
+  display: flex;
+`;
+
+const BannerImage = styled.img`
+  width: 100%;
+`;
 
 export default HomeBanner;
